3d6276da48628e652ead501d006743bb68cfc31a
caiym <1152545264@qq.com>
2025-02-05 18:09:36 +0800

    api服务对接rpc服务

--- 3goZeroBasic/api/internal/logic/userlogic.go
+++ 3goZeroBasic/api/internal/logic/userlogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"goZeroStudy/3goZeroBasic/user/userclient"
 
 	"goZeroStudy/3goZeroBasic/api/internal/svc"
 	"goZeroStudy/3goZeroBasic/api/internal/types"
@@ -25,6 +26,21 @@ func NewUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLogic {
 
 func (l *UserLogic) User(req *types.UserReq) (resp *types.UserResp, err error) {
 	// todo: add your logic here and delete this line
-	resp = &types.UserResp{Id: "666", Name: "test", Phone: "testPhone"}
+	//resp = &types.UserResp{Id: "666", Name: "test", Phone: "testPhone"}
+
+	getUserResp, err := l.svcCtx.UserClient.GetUser(l.ctx, &userclient.GetUserReq{
+		Id: req.Id,
+	})
+	if err != nil {
+		return nil, err
+	}
+	res := &types.UserResp{
+		Id:    getUserResp.Id,
+		Name:  getUserResp.Name,
+		Phone: getUserResp.Phone,
+	}
+	return res, nil
+
 	return
+
 }
--- 3goZeroBasic/api/internal/svc/servicecontext.go
+++ 3goZeroBasic/api/internal/svc/servicecontext.go
@@ -1,15 +1,20 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/zrpc"
 	"goZeroStudy/3goZeroBasic/api/internal/config"
+	"goZeroStudy/3goZeroBasic/user/userclient"
 )
 
 type ServiceContext struct {
 	Config config.Config
+
+	UserClient userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		UserClient: userclient.NewUser(zrpc.MustNewClient(c.UserRPC)),
 	}
 }
